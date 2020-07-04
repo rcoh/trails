aef83d4938aab6fc862114620f7e12856610b9da
Russell Cohen <russell.r.cohen@gmail.com>
2020-07-04 00:35:45 -0400

    Configure admin site

--- trails/est/admin.py
+++ trails/est/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from est.models import Import
+
+
+class ImportAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Import, ImportAdmin)
+
 # Register your models here.
